ccbc9d3fb476a48dbe6db4d97eaa0cd80ac8246e
Asus <Asus@DESKTOP-LPTQR5H.fritz.box>
2016-12-25 13:41:59 +0100

    controller schnittstellen methoden definiert. dummy inhalte

--- Filmliste/src/main/java/main/Controller.java
+++ Filmliste/src/main/java/main/Controller.java
@@ -1,26 +1,71 @@
 package main;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	
+	@RequestMapping(value="/filme/{filmname}", method=RequestMethod.GET)
+	public HttpEntity<Film> getFilm(@PathVariable String filmname){
+		String c = "GET irgendein film";
+		Film film = new Film(c);
+		film.add(linkTo(methodOn(Controller.class).getFilm(filmname)).withSelfRel());
+		return new ResponseEntity<Film>(film, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/filme/{filmname}", method=RequestMethod.DELETE)
+	public HttpEntity<Film> deleteFilm(@PathVariable String filmname){
+		String c = "DELETE irgendein film";
+		Film film = new Film(c);
+		film.add(linkTo(methodOn(Controller.class).deleteFilm(filmname)).withSelfRel());
+		return new ResponseEntity<Film>(film, HttpStatus.OK);
+	}
+	
+	//create
+	@RequestMapping(value="/filme/{filmname}", method=RequestMethod.PUT)
+	public HttpEntity<Film> putFilm(@PathVariable String filmname){
+		String c = "PUT irgendein film";
+		Film film = new Film(c);
+		film.add(linkTo(methodOn(Controller.class).putFilm(filmname)).withSelfRel());
+		return new ResponseEntity<Film>(film, HttpStatus.OK);
+	}
+	
+	//update
+	@RequestMapping(value="/filme/{filmname}", method=RequestMethod.POST)
+	public HttpEntity<Film> postFilm(@PathVariable String filmname,
+			@RequestParam(value="name", required=false, defaultValue="") String newname
+			){
+		String name = newname.isEmpty() ? filmname : newname;
+		String c = "POST irgendein film mit neuem namen : "+name;
+		Film film = new Film(c);
+		film.add(linkTo(methodOn(Controller.class).postFilm(name, name)).withSelfRel());
+		return new ResponseEntity<Film>(film, HttpStatus.OK);
+	}
 
-    private static final String TEMPLATE = "Hello, %s!";
+    @RequestMapping(value="/filmliste", method=RequestMethod.GET)
+    public HttpEntity<Filmliste> filmliste() {
 
-    @RequestMapping("/film")
-    public HttpEntity<Film> film(
-            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-
-        Film film = new Film(String.format(TEMPLATE, name));
-        film.add(linkTo(methodOn(Controller.class).film(name)).withSelfRel());
-
-        return new ResponseEntity<Film>(film, HttpStatus.OK);
+    	List<String> elements = new LinkedList<>();
+    	elements.add("three");
+    	elements.add("two");
+        Filmliste film = new Filmliste(elements);
+        film.add(linkTo(methodOn(Controller.class).filmliste()).withSelfRel());
+        
+        return new ResponseEntity<Filmliste>(film, HttpStatus.OK);
     }
+    
+    
 }
